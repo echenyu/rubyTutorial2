f1c2a699cbb140693d94730cd0779460480e7471
Eric Yu <echenyu@umich.edu>
2016-05-17 15:14:42 -0700

    Added welcome controller and welcome view. Basic html page to test out rails

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,8 +1,5 @@
-class ApplicationController < ActionController::Base
+class WelcomeController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
-  def hello
-  	render text: "Hi!"
-  end
 end
