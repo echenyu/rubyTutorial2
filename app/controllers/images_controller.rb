e6f5ac8d7f0d83e29eb3639dbc381be18490ede0
Eric Yu <echenyu@umich.edu>
2016-05-20 15:03:34 -0700

    ey - added image link validation and tests

--- app/controllers/images_controller.rb
+++ app/controllers/images_controller.rb
@@ -1,3 +1,6 @@
+require 'uri'
+require 'net/http'
+
 class ImagesController < ApplicationController
   def index
     @images = Image.all
@@ -11,12 +14,35 @@ class ImagesController < ApplicationController
   end
 
   def create
-    @image = Image.new(image_params)
-    @image.save!
-    redirect_to images_path
+    if valid_image_link?(params[:image][:link])
+      @image = Image.new(image_params)
+      @image.save!
+      redirect_to images_path
+    else
+      flash[:notice] = 'Make sure your link goes to an image'
+      redirect_to new_image_path
+    end
   end
 
   private
+
+  def valid_link?(link)
+    uri = URI.parse(link)
+    uri.is_a?(URI::HTTP)
+  rescue URI::InvalidURIError
+    false
+  end
+
+  def valid_image_link?(link)
+    if valid_link?(link)
+      url = URI.parse(link)
+      Net::HTTP.start(url.host, url.port) do |http|
+        return http.head(url.request_uri)['Content-Type'].start_with? 'image'
+      end
+    else
+      false
+    end
+  end
 
   def image_params
     params.require(:image).permit(:user_name, :link)
